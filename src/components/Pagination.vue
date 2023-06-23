967330fdc7a09881d93985128ffc2eb425e80d6b
vorleaknath <vorleakgl@gmail.com>
2023-06-23 21:52:57 +0700

    pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="flex justify-center bg-white h-20 m-4 rounded mb-10 ">
+    <ul class="inline-flex py-4 text-sm font-normal">
+      <li>
+        <button v-if="showPreviousPage" @click="updatePage(currentPage -1)" class="bg-gray-100 px-2 py-2 rounded-l-lg border border-gray-300 hover:bg-gray-300">Previous </button>
+      </li>
+      <li>
+        <button class="text-sm font-normal px-4 border border-gray-300 py-2">
+          {{ currentPage + 1 }} of {{ totalPages() }} Pages
+        </button>
+      </li>
+      <li>
+        <button v-if="showNextPage" @click="updatePage(currentPage + 1)" class="bg-gray-100 px-2 py-2 rounded-r-lg border border-gray-300 hover:bg-gray-300 ">Previous</button>
+      </li>
+    
+  </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Pagination',
+  props: {
+    countries: Array,
+    totalRows: Number,
+    currentPage: Number,
+    pageSize: Number
+  }, 
+  methods: {
+    updatePage(pageNumber){
+      this.$emit('update-page', pageNumber)
+    },
+    totalPages() {
+      return Math.ceil(this.totalRows / this.pageSize)
+    },
+    showPreviousPage() {
+      return this.currentPage == 0 ? false : true
+    },
+    showNextPage() {
+      return this.currentPage == (this.totalPages() - 1) ? false : true
+    }
+  },
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
-    <Header @btn-search="searchCountries" />
     <div class="flex justify-center text-gray-700">
       <div class="w-2/3 md:w-2/3 sm:w-5/6">
-        <Catalogs :countries="countries"/>
+        <Catalogs :countries="visibleCountries"/>
+        <Pagination 
+          :currentPage="currentPage" 
+          :totalRows="totalRows"
+          :pageSize="pageSize"
+          @update-page="updatePage"
+        />
       </div>
     </div>
   </div>
@@ -12,29 +17,46 @@
 <script>
 import axios from 'axios'
 import Catalogs from '../components/Catalogs.vue'
+import Pagination from '../components/Pagination.vue'
 
 export default {
   name: 'HomeView',
   data() {
     return {
       countries: [],
+      visibleCountries: [],
+      currentPage: 0,
+      pageSize: 25,
+      totalRows:0,
     }
   },
   components: {
     Catalogs,
+    Pagination,
   },
   methods: {
 
-    // Get Countries 
+    // Get Countries and display 25 rows per page
     async fetchCountries() {
       await axios
         .get('https://restcountries.com/v3.1/all')
         .then((response) => {
           this.countries = response.data
+          this.totalRows = this.countries.length
+          this.visibleCountries = this.countries.slice( this.currentPage * this.pageSize, (this.pageSize * this.currentPage) + this.pageSize)
+
+        if(this.visibleCountries.length == 0 && this.currentPage > 0) {
+          this.updatePage(this.currentPage - 1 )
+        }
         })
         .catch((error) => {
           console.error(error)
         })
+    },
+    // Update page on pagination
+    updatePage(pageNumer) {
+      this.currentPage = pageNumer
+      this.fetchCountries()
     },
   },
   created() {
