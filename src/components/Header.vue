f6209f9f0730129a9497ffe41316104c596cb79f
vorleaknath <vorleakgl@gmail.com>
2023-06-23 21:54:18 +0700

    search

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-center bg-white h-24 px-4 shadow">
+    <form @submit="onSubmit" class="w-2/3 md:w-2/3 sm:w-5/6 my-6 mx-4 bg-gray-100 rounded  hover:shadow">
+      <div class="flex justify-between ">
+        <input type="text" v-model="searchQuery" placeholder="Enter Country Name..." class="bg-inherit text-sm text-normal text-gray-600 py-2 px-4 focus:outline-none w-full">
+        <input type="submit"  value="Search" class="bg-gray-500 hover:bg-inherit hover:text-gray-700 text-white py-3 px-3 rounded"/>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      searchQuery: ''
+    }
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault()
+
+      const searchQuery = this.searchQuery
+
+      this.$emit('btn-search', searchQuery)
+      this.searchQuery = ''
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
+    <Header @btn-search="searchCountries" />
     <div class="flex justify-center text-gray-700">
       <div class="w-2/3 md:w-2/3 sm:w-5/6">
+        <!-- Sort -->
+        <div class="flex justify-end p-5">
+          <label class="text-sm pr-5">Sort by Country Name:</label>
+          <select v-model="sortOrder" @change="sortCountries" class="bg-gray-100 text-sm focus:outline-none">
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
+        </div>
         <Catalogs :countries="visibleCountries"/>
         <Pagination 
           :currentPage="currentPage" 
@@ -16,6 +25,7 @@
 
 <script>
 import axios from 'axios'
+import Header from '../components/Header.vue'
 import Catalogs from '../components/Catalogs.vue'
 import Pagination from '../components/Pagination.vue'
 
@@ -24,6 +34,7 @@ export default {
   data() {
     return {
       countries: [],
+      searchQuery: '',
       visibleCountries: [],
       currentPage: 0,
       pageSize: 25,
@@ -33,6 +44,7 @@ export default {
   components: {
     Catalogs,
     Pagination,
+    Header,
   },
   methods: {
 
@@ -57,6 +69,23 @@ export default {
     updatePage(pageNumer) {
       this.currentPage = pageNumer
       this.fetchCountries()
+    },
+
+    // Search Countries by name
+    async searchCountries(query) {
+      if(query !== '') {
+        await axios
+          .get(`https://restcountries.com/v3.1/name/${query}`)
+          .then(response => {
+            this.totalRows = response.data.length
+            this.visibleCountries = response.data.slice(
+            this.currentPage * this.pageSize,(this.pageSize * this.currentPage) + this.pageSize
+          )
+        })
+        .catch(error => {
+          console.error(error)
+        })
+      }
     },
   },
   created() {
